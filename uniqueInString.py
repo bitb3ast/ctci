cb4e2242b60b4a8053ab369b7b9bbdad4d3e70a5
bitb3ast <jerrinsuresh@gmail.com>
2017-07-15 13:42:53 -0400

    BugFix Unique Characters in String

--- uniqueInString.py
+++ uniqueInString.py
@@ -1,11 +1,16 @@
 #check if a string has unique characters
 
-occurance = {}
+def isUnique(string):
+	occurance = {}
+	for char in string:
+		occurance.setdefault(char,0)
+		occurance[char] += 1
+		if occurance[char] > 1:
+			return 'Repeating Characters in String'
 
-for char in input():
-	occurance.setdefault(char,0)
-	occurance[char] += 1
-	if occurance[char] > 1:
-		print('Repeating Characters in String')
-	else:
-		print('Unique Characters in String')
+	return 'String has unique characters'
+
+
+string = raw_input('Enter String ')
+print(isUnique(string))
+	
