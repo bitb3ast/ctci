fb6820192a106cb0d5671cd5c4f6c7b4852caa3a
bitb3ast <jerrinsuresh@gmail.com>
2017-07-15 22:45:22 -0400

    Character Permuations

---
+++ stringIsPermutation.py
@@ -0,0 +1,6 @@
+#check if a string one is a permuation of string two
+
+if (sorted(raw_input('First String ')) == sorted(raw_input('Second String '))):
+	print('Bingo!! The strings are permuations of same characters')
+else:
+	print('Strings are not permuations of same characters')
